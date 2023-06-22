51e5ebd362dd997dd0e8cd168d1eb8fa45b7a735
João Fernandes <jguiborges@gmail.com>
2023-06-22 08:07:29 -0300

    Enviando arquivos

---
+++ src/main/java/com/ifsul/trabalhovolei/TrabalhoVoleiApplication.java
@@ -0,0 +1,13 @@
+package com.ifsul.trabalhovolei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoVoleiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoVoleiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifsul/trabalhovolei/TrabalhoVoleiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsul.trabalhovolei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoVoleiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
